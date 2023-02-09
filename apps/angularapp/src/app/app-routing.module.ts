9d21b35af7ae200e515e0592db6e62d41817ae55
neha <neha.poddar@knoldus.com>
2023-02-09 13:04:19 +0530

    removed component

--- apps/angularapp/src/app/app-routing.module.ts
+++ apps/angularapp/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { ReadModeComponent } from './Components/read-mode/read-mode.component';
-import { RecipyListComponent } from './Components/recipy-list/recipy-list.component';
 
 const routes: Routes = [
   {
@@ -11,18 +10,18 @@ const routes: Routes = [
   {
     path: 'home', component: HomeComponent
   },
-  { path: 'recipyList', component: RecipyListComponent },
-  {
-    path: 'recipyList',
-    children: [
-      {
-        path: 'readMode/:id',
-        component: ReadModeComponent,
-      }
-      
-    ],
-    
-  },
+  // { path: 'recipyList', component: RecipyListComponent },
+  // {
+  //   path: 'recipyList',
+  //   children: [
+  //     {
+  //       path: 'readMode/:id',
+  //       component: ReadModeComponent,
+  //     }
+
+  //   ],
+
+  // },
 
   {
     path: '**',
--- apps/angularapp/src/app/app.module.ts
+++ apps/angularapp/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './Components/add-product/add-product.component';
 import { HomeComponent } from './Components/home/home.component';
-import { RecipyListComponent } from './Components/recipy-list/recipy-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -17,7 +16,6 @@ import {MatDialogModule} from '@angular/material/dialog';
  import { HttpClientModule } from '@angular/common/http';
 import { ReadModeComponent } from './Components/read-mode/read-mode.component';
 import {MatSelectModule} from '@angular/material/select';
-import { CommonDataModule } from './shared/common-data/common-data.module';
 import {MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material/snack-bar';
 
 @NgModule({
@@ -25,7 +23,6 @@ import {MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/materia
     AppComponent,
     AddProductComponent,
     HomeComponent,
-    RecipyListComponent,
     ReadModeComponent
 
   ],
@@ -45,7 +42,6 @@ import {MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/materia
     FormsModule,
     MatIconModule,
     HttpClientModule,
-    CommonDataModule,
     MatSnackBarModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
